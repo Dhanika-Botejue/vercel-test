ee329358144fab804ce95ec465ea1f0168f9ec83
Dhanika Botejue <dhanika.botejue@gmail.com>
2025-08-06 20:15:15 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello, World - This is a test to know how to deploy with vercel"
+
+if __name__ == "__main__":
+    app.run()
